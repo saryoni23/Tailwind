22afe06b979f5c8d5d5e77414c69f2dc0ffd6f65
saryoni23 <indrasaryoni@gmail.com>
2022-04-17 21:54:45 +0700

    1

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: ['./public/**/*.{html,js}'],
+  // enable dark mode via class strategy
+  darkMode: 'media',
+  theme: {
+    container: {
+      center: true,
+      padding: '1rem',
+    },
+    extend: {
+      colors: {
+        'abutua': '#3A405A',
+        'abumuda': '#AEC5EB',
+        'coklatsusu': '#F9DEC9',
+        'coklatmuda': '#E9AFA3',
+        'coklat': '#685044',
+      },
+    },
+    
+  },
+  plugins: [],
+}
